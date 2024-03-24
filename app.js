05bc14b96a4f9eff1a8272c2b17a3b2430ddf77d
alongchamps <a.j.longchamps@gmail.com>
2024-03-23 20:28:11 -0400

    Tried adding an endpoint for /weather

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Last push for tonight'));
 
+app.get('/weather', (req, res) => res.send('Getting weather for...'));
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
